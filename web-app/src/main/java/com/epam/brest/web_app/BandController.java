bae3517cd7cffa2f3141b4fa0dfdf7115ba4b1d1
Maksim Meliashchuk <maksym873@gmail.com>
2021-11-06 23:34:28 +0300

    add simple test band html template

--- web-app/src/main/java/com/epam/brest/web_app/BandController.java
+++ web-app/src/main/java/com/epam/brest/web_app/BandController.java
@@ -6,17 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
- * Hello MVC controller.
+ * MVC controller.
  */
 @Controller
 public class BandController {
 
-    @GetMapping(value = "/hello")
-    public String hello(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
+    @GetMapping(value = "/band")
+    public String hello(@RequestParam(value = "name", required = false, defaultValue = "") String name,
                         Model model) {
 
         System.out.println("hello(name:'" + name + "')");
         model.addAttribute("name", name);
-        return "hello";
+        return "band";
     }
 }
