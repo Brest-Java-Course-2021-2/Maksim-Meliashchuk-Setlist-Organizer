fdce2cf89924bc7bab49a3f23881d09f9713beb9
Maksim Meliashchuk <maksym873@gmail.com>
2021-12-07 23:17:44 +0300

    add VersionController

---
+++ rest-app/src/main/java/com/epam/brest/rest/VersionController.java
@@ -0,0 +1,14 @@
+package com.epam.brest.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersionController {
+    private final static String VERSION = "0.0.1";
+
+    @GetMapping(value = "/version")
+    public String version() {
+        return VERSION;
+    }
+}
