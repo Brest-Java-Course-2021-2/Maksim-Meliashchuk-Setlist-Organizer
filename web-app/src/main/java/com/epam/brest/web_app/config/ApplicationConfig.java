411ce4d1c7f036d2a6e2f8e670297fb08dca2168
Maksim Meliashchuk <maksym873@gmail.com>
2022-01-18 01:08:50 +0300

    add LocalDate deserializer with date format from properties

--- web-app/src/main/java/com/epam/brest/web_app/config/ApplicationConfig.java
+++ web-app/src/main/java/com/epam/brest/web_app/config/ApplicationConfig.java
@@ -1,14 +1,25 @@
 package com.epam.brest.web_app.config;
 
 import com.epam.brest.service.*;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @ComponentScan
 @Profile("dev")
+@PropertySource("classpath:application-dev.properties")
 @PropertySource("classpath:application-dev.properties")
 public class ApplicationConfig {
 
@@ -19,9 +30,22 @@ public class ApplicationConfig {
     @Value("${rest.server.port}")
     private Integer port;
 
+    @Value("${spring.jackson.date-format}")
+    private String dateFormat;
+
     @Bean
-    RestTemplate restTemplate() {
-        return new RestTemplate(new SimpleClientHttpRequestFactory());
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate(new SimpleClientHttpRequestFactory());
+        List<HttpMessageConverter<?>> converters = new ArrayList<>();
+        DateTimeFormatterBuilder dateTimeFormatterBuilder = new DateTimeFormatterBuilder()
+                .appendOptional(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+                .appendOptional(DateTimeFormatter.ofPattern(dateFormat));
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+        builder.deserializers(new LocalDateDeserializer(dateTimeFormatterBuilder.toFormatter()));
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter(builder.build());
+        converters.add(jsonConverter);
+        restTemplate.setMessageConverters(converters);
+        return restTemplate;
     }
 
     @Bean
