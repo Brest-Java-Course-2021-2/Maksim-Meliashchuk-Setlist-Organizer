4b0734423d8cda3475757007b1d393abdbfac3bb
Maksim Meliashchuk <maksym873@gmail.com>
2022-02-09 16:45:17 +0300

    update ApplicationHerokuConfig

--- web-app/src/main/java/com/epam/brest/web_app/config/ApplicationHerokuConfig.java
+++ web-app/src/main/java/com/epam/brest/web_app/config/ApplicationHerokuConfig.java
@@ -2,9 +2,11 @@ package com.epam.brest.web_app.config;
 
 import com.epam.brest.service.*;
 import com.epam.brest.web_app.condition.RestTemplateCondition;
+import com.epam.brest.web_app.condition.WebClientCondition;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 @ComponentScan
@@ -19,8 +21,11 @@ public class ApplicationHerokuConfig {
 
     private final RestTemplate restTemplate;
 
-    public ApplicationHerokuConfig(RestTemplate restTemplate) {
+    private final WebClient webClient;
+
+    public ApplicationHerokuConfig(RestTemplate restTemplate, WebClient webClient) {
         this.restTemplate = restTemplate;
+        this.webClient = webClient;
     }
 
     @Bean
@@ -31,10 +36,22 @@ public class ApplicationHerokuConfig {
     }
 
     @Bean
+    @Conditional(WebClientCondition.class)
+    BandDtoService bandDtoServiceWebClient() {
+        return new BandDtoServiceWebClient("/bands_dto", webClient);
+    }
+
+    @Bean
     @Conditional(RestTemplateCondition.class)
     BandService bandService() {
         String url = String.format("%s://%s/bands", protocol, host);
         return new BandServiceRest(url, restTemplate);
+    }
+
+    @Bean
+    @Conditional(WebClientCondition.class)
+    BandService bandServiceWebClient() {
+        return new BandServiceWebClient("/bands", webClient);
     }
 
     @Bean
@@ -45,9 +62,21 @@ public class ApplicationHerokuConfig {
     }
 
     @Bean
+    @Conditional(WebClientCondition.class)
+    TrackService trackServiceWebClient() {
+        return new TrackServiceWebClient("/repertoire", webClient);
+    }
+
+    @Bean
     @Conditional(RestTemplateCondition.class)
     TrackDtoService trackDtoService() {
         String url = String.format("%s://%s/repertoire/filter", protocol, host);
         return new TrackDtoServiceRest(url, restTemplate);
+    }
+
+    @Bean
+    @Conditional(WebClientCondition.class)
+    TrackDtoService trackDtoServiceWebClient() {
+        return new TrackDtoServiceWebClient("/repertoire/filter", webClient);
     }
 }
