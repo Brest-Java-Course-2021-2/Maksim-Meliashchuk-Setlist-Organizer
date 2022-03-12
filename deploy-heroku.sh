cb2865e43f56b3bd510d7bd23b23222fe43e5eef
Maksim Meliashchuk <maksym873@gmail.com>
2022-03-12 13:16:42 +0300

    update deploy-heroku.sh

--- deploy-heroku.sh
+++ deploy-heroku.sh
@@ -13,8 +13,10 @@ docker build ./web-app -t getting-started
 
 # Start new deployment
 cd rest-app
+heroku container:login
 heroku container:push web --app setlist-organizer-rest
 heroku container:release web --app setlist-organizer-rest
 cd ../web-app
+heroku container:login
 heroku container:push web --app setlist-organizer-web
 heroku container:release web --app setlist-organizer-web
