079b12025dcb209f9313fcb01cbd71b4679745b4
Maksim Meliashchuk <maksym873@gmail.com>
2022-07-03 23:27:25 +0300

    added content auto-reloading in the Web layer

--- web-app/src/main/resources/static/js/main.js
+++ web-app/src/main/resources/static/js/main.js
@@ -4,11 +4,25 @@ let client = Stomp.over(sock);
 
 client.connect({}, (frame) => {
 	console.log("Frame is: " + frame);
+	var path = window.location.pathname;
+    var page = path.split("/").pop();
 	client.subscribe('/topic/repertoire', (payload) => {
-	var e = document.getElementById("toastNotice");
-    var toast = new bootstrap.Toast(e, option);
-    var body = document.getElementById("t-body")
-    body.innerHTML = payload.body
-    toast.show();
+	    if (page == "bands" || page == "repertoire") {
+                console.log("Reloaded page name is: " + page);
+                updateContent();
+            }
+	    var e = document.getElementById("toastNotice");
+        var toast = new bootstrap.Toast(e, option);
+        var body = document.getElementById("t-body")
+        body.innerHTML = payload.body
+        toast.show();
 	});
 });
+
+function updateContent()
+{
+    $("#reloaded-content").load(window.location.href + " #reloaded-content", function() {
+        secondsToTime();
+        dateFormat();
+    });
+}
